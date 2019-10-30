0a10d8e84689df872d911dcc70ee8b0876127c72
9977559 <dann.hollander@gmail.com>
2019-10-30 19:52:42 +1300

    maybe finished?

--- js.js
+++ js.js
@@ -1,20 +1,15 @@
-// There are many ways to pick a DOM node; here we get the form itself and the email
-// input box, as well as the span element into which we will place the error message.
-
 var form = document.getElementsByTagName("form")[0];
 var email = document.getElementById("mail");
-var error = document.querySelector(".error");
-
+var error = document.querySelector(".error");	
+//Email Validation Code
 email.addEventListener(
 	"input",
 	function(event) {
-		// Each time the user types something, we check if the
-		// email field is valid.
+		
 		if (email.validity.valid) {
-			// In case there is an error message visible, if the field
-			// is valid, we remove the error message.
-			error.innerHTML = ""; // Reset the content of the message
-			error.className = "error"; // Reset the visual state of the message
+		
+			error.innerHTML = ""; 
+			error.className = "error"; 
 		}
 	},
 	false
@@ -22,16 +17,19 @@ email.addEventListener(
 form.addEventListener(
 	"submit",
 	function(event) {
-		// Each time the user tries to send the data, we check
-		// if the email field is valid.
+	
 		if (!email.validity.valid) {
-			// If the field is not valid, we display a custom
-			// error message.
+			
 			error.innerHTML = "Please enter a vailid email";
 			error.className = "error active";
-			// And we prevent the form from being sent by canceling the event
+			
 			event.preventDefault();
 		}
+	
 	},
 	false
 );
+/*Function to print email form to screen*/
+function myFunction() {
+	document.getElementById("field2").value = document.getElementById("mail").value;
+  }
